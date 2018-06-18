e250e8a777a0002cf89bc43ae40c117c7adedd0f
ABardot <aabardot5@gmail.com>
2018-06-17 23:22:08 -0400

    Added functionality to Added page with validation and error message and success message. Page has added items and works!

--- CmsShoppingCart/CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -33,5 +33,63 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
         {
             return View();
         }
+
+        // GET: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            // Check Model State
+            if (! ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+
+
+                // Declare Slug
+                string slug;
+                
+                // Initiate pageDTO
+                PageDTO dto = new PageDTO();
+
+                // DTO Title
+                dto.Title = model.Title;
+
+                // Check for and set slug are if need be
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                // Make sure title and slug are unique
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "That title or slug already exists");
+                    return View(model);
+                }
+
+                // DTO the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSideBar = model.HasSideBar;
+                dto.Sorting = 100;
+
+                // Save DBO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            // Set TempData message
+            TempData["SM"] = "You have added a new page!";
+
+            // Redirect
+            return RedirectToAction("AddPage");
+        }
     }
 }
