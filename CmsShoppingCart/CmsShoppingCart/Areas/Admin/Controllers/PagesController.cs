992ecfb84617da4de97725c5389faa1553987dd2
ABardot <aabardot5@gmail.com>
2018-06-17 22:16:43 -0400

    First Commit, added nuget packages and Entity Database

---
+++ CmsShoppingCart/CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,30 @@
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            // Declare List of view PageVm
+            List<PageVM> pagesList;
+           
+            using (Db db = new Db())
+            {
+                // Initiate the list, new PageVM is sent by the ctor in the controller. ToArrya is needed or Entity
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            // Return view with list
+
+            return View(pagesList);
+        }
+    }
+}
---
+++ CmsShoppingCart/CmsShoppingCart/Models/ViewModels/Pages/PageVM.cs
@@ -0,0 +1,37 @@
+﻿using CmsShoppingCart.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CmsShoppingCart.Models.ViewModels.Pages
+{
+    public class PageVM
+    {
+        public PageVM()
+        {
+            // Default
+        }
+
+        public PageVM(PageDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Sorting = row.Sorting;
+            HasSideBar = row.HasSideBar;
+        }
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HasSideBar { get; set; }
+    }
+}
